6f2386e036a399dc08bf152453d6ea8a27379266
Your Name <maryxiong@xiongqingfeideMacBook-Pro.local>
2023-04-13 22:41:22 +0800

    feat: home

---
+++ source/.vuepress/components/intro-comp.vue
@@ -0,0 +1,71 @@
+<template>
+    <div class="intro">
+        <div v-for="el in list" class="item" @click="goto(el.link)">
+            <h2>{{ el.h2 }}</h2>
+            <p>{{ el.p }}</p>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            list: [
+                {
+                    h2: '奥运普及',
+                    p: '补充一些描述',
+                    link: '/doc/olympics',
+                },
+                {
+                    h2: '场馆建设',
+                    p: '补充一些描述',
+                    link: '/doc/venue',
+                },
+                {
+                    h2: '高水平训练',
+                    p: '补充一些描述',
+                    link: '/doc/train',
+                },
+                {
+                    h2: '赛事运营',
+                    p: '补充一些描述',
+                    link: '/doc/operate',
+                },
+            ],
+        }
+    },
+    methods: {
+        goto(link) {
+            location.href = link;
+        }
+    }
+}
+</script>
+<style>
+.intro {
+    margin-top: 60px;
+    border-top: 1px solid #ddd;
+}
+.intro h2 {
+    font-size: 1.4rem;
+    font-weight: 500;
+    border-bottom: none;
+    padding-bottom: 0;
+    color: #3a5169;
+}
+.intro  p {
+    color: #4e6e8e;
+}
+.intro .item {
+    display: inline-block;
+    width: 49%;
+    cursor: pointer;
+}
+.intro .item:hover h2,
+.intro .item:hover p
+ {
+    color: #f30;
+    opacity: 0.8;
+    text-decoration: underline;
+}
+</style>
